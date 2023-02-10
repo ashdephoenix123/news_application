43db456e56764333b823301e280e4b1613d35d2f
akash <akashsarki12345@gmail.com>
2023-02-10 11:59:04 +0530

    navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,14 +5,14 @@ const Navbar = () => {
     return (
         <>
             <nav className="navbar navbar-expand-xl bg-body-secondary py-3">
-                <div className="container-fluid navbar-font navbar-font-2">
-                    <Link className="navbar-brand navbar-font-1 mx-5" to="/">NEWZAPP</Link>
+                <div className="container-fluid navbar-font navbar-font-2 mx-4">
+                    <Link className="navbar-brand navbar-font-1" to="/">NEWZAPP</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div className="collapse navbar-collapse text-center" id="navbarSupportedContent">
+                    <div className="collapse navbar-collapse text-center justify-content-end" id="navbarSupportedContent">
 
-                        <ul className="navbar-nav mb-lg-0 mx-auto">
+                        <ul className="navbar-nav mb-lg-0">
                             <li className="nav-item">
                                 <Link className="nav-link" to="/">Home</Link>
                             </li>
